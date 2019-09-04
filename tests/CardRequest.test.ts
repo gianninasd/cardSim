504944c175bb6bc0577372f9644d2ae8c44713cd
gianninasd <angelone55@hotmail.com>
2019-09-03 21:52:47 -0400

    added unit test

--- tests/CardRequest.test.ts
+++ tests/CardRequest.test.ts
@@ -26,3 +26,24 @@ test("determine AM brand", () => {
   let brand = CardRequest.parseBrand(req.cardNum);
   expect(brand).toBe("AM");
 });
+
+test("return default data when paymentToken", () => {
+  let rawData:object = {
+    merchantRefNum: "ref-1",
+    amount: 1500,
+    settleWithAuth: true,
+    card: {
+      paymentToken: "5d6efecd9f4343"
+    },
+    billingDetails: {
+      zip: "H8P3S2"
+    }
+  };
+  
+  let cardReq:CardRequest = CardRequest.parse("some-id", rawData);
+  expect(cardReq.id).toBe("some-id");
+  expect(cardReq.merchantRefNum).toBe("ref-1");
+  expect(cardReq.amount).toBe(1500);
+  expect(cardReq.cardBrand).toBe("MC");
+  expect(cardReq.cardNum).toBe("5500000000000004");
+});
